6520d4e4b0252ee93cc013197576bc140c3e7895
grey.c.rus <grey.c.rus@localhost>
2013-05-03 23:35:15 +0400

    Remove ArDrone -only ARM!

--- ARDroneLib/Soft/Build/targets_versions/ardrone_lib_PROD_MODE_vlib_Linux_3.5.0-7-lowlatency_GNU_Linux_usrbingcc_/ardrone_tool/Video/video_stage_ffmpeg_recorder.c.d
+++
@@ -1,2 +0,0 @@
-video_stage_ffmpeg_recorder.o ../../Soft/Build/targets_versions/ardrone_lib_PROD_MODE_vlib_Linux_3.5.0-7-lowlatency_GNU_Linux_usrbingcc_/ardrone_tool/Video/video_stage_ffmpeg_recorder.c.d: \
- ../..//Soft/Lib/ardrone_tool/Video/video_stage_ffmpeg_recorder.c
