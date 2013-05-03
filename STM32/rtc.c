b328e042d75f9240e27ddbed1c2a6ef51a05d59f
valentin <valentin@localhost>
2013-05-03 23:23:29 +0400

    Add STM32F3 Sort other

--- STM32/rtc.c
+++ STM32/rtc.c
@@ -5,6 +5,7 @@
 /*--------------------------------------------------------------------------  */
 
 #include "stm32f10x.h"
+//#include "stm32f2xx.h"
 //#include "stm32_eval.h"
 #include "rtc.h"
 
@@ -216,9 +217,9 @@ void rtc_gettime (RTC_t *rtc)
 {
 	uint32_t t;
 
-	while ( ( t = RTC_GetCounter() ) != RTC_GetCounter() ) { ; }
-	counter_to_struct( t, rtc ); // get non DST time
-	adjustDST( rtc );
+//	while ( ( t = RTC_GetCounter() ) != RTC_GetCounter() ) { ; }
+//	counter_to_struct( t, rtc ); // get non DST time
+//	adjustDST( rtc );
 }
 
 /*******************************************************************************
@@ -230,12 +231,12 @@ void rtc_gettime (RTC_t *rtc)
 *******************************************************************************/
 static void my_RTC_SetCounter(uint32_t cnt)
 {
-	/* Wait until last write operation on RTC registers has finished */
-	RTC_WaitForLastTask();
-	/* Change the current time */
-	RTC_SetCounter(cnt);
-	/* Wait until last write operation on RTC registers has finished */
-	RTC_WaitForLastTask();
+//	/* Wait until last write operation on RTC registers has finished */
+//	RTC_WaitForLastTask();
+//	/* Change the current time */
+//	RTC_SetCounter(cnt);
+//	/* Wait until last write operation on RTC registers has finished */
+//	RTC_WaitForLastTask();
 }
 
 /*******************************************************************************
@@ -276,63 +277,63 @@ int rtc_init(void)
 	volatile uint16_t i;
 
 	/* Enable PWR and BKP clocks */
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE);
-
-	/* LSI clock stabilization time */
-	for(i=0;i<5000;i++) { ; }
-
-	if (BKP_ReadBackupRegister(BKP_DR1) != 0xA5A5) {
-		/* Backup data register value is not correct or not yet programmed (when
-		   the first time the program is executed) */
-
-		/* Allow access to BKP Domain */
-		PWR_BackupAccessCmd(ENABLE);
-
-		/* Reset Backup Domain */
-		BKP_DeInit();
-
-		/* Enable LSE */
-		RCC_LSEConfig(RCC_LSE_ON);
-
-		/* Wait till LSE is ready */
-
-		while (RCC_GetFlagStatus(RCC_FLAG_LSERDY) == RESET) { ; }
-
-		/* Select LSE as RTC Clock Source */
-		RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);
-
-		/* Enable RTC Clock */
-		RCC_RTCCLKCmd(ENABLE);
-
-		/* Wait for RTC registers synchronization */
-		RTC_WaitForSynchro();
-
-		/* Wait until last write operation on RTC registers has finished */
-		RTC_WaitForLastTask();
-
-		/* Set RTC prescaler: set RTC period to 1sec */
-		RTC_SetPrescaler(32767); /* RTC period = RTCCLK/RTC_PR = (32.768 KHz)/(32767+1) */
-
-		/* Wait until last write operation on RTC registers has finished */
-		RTC_WaitForLastTask();
-
-		/* Set initial value */
-		RTC_SetCounter( (uint32_t)((11*60+55)*60) ); // here: 1st January 2000 11:55:00
-
-		/* Wait until last write operation on RTC registers has finished */
-		RTC_WaitForLastTask();
-
-		BKP_WriteBackupRegister(BKP_DR1, 0xA5A5);
-
-		/* Lock access to BKP Domain */
-		PWR_BackupAccessCmd(DISABLE);
-
-	} else {
-
-		/* Wait for RTC registers synchronization */
-		RTC_WaitForSynchro();
-
-	}
+//	RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE);
+//
+//	/* LSI clock stabilization time */
+//	for(i=0;i<5000;i++) { ; }
+//
+//	if (BKP_ReadBackupRegister(BKP_DR1) != 0xA5A5) {
+//		/* Backup data register value is not correct or not yet programmed (when
+//		   the first time the program is executed) */
+//
+//		/* Allow access to BKP Domain */
+//		PWR_BackupAccessCmd(ENABLE);
+//
+//		/* Reset Backup Domain */
+//		BKP_DeInit();
+//
+//		/* Enable LSE */
+//		RCC_LSEConfig(RCC_LSE_ON);
+//
+//		/* Wait till LSE is ready */
+//
+//		while (RCC_GetFlagStatus(RCC_FLAG_LSERDY) == RESET) { ; }
+//
+//		/* Select LSE as RTC Clock Source */
+//		RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);
+//
+//		/* Enable RTC Clock */
+//		RCC_RTCCLKCmd(ENABLE);
+//
+//		/* Wait for RTC registers synchronization */
+//		RTC_WaitForSynchro();
+//
+//		/* Wait until last write operation on RTC registers has finished */
+//		RTC_WaitForLastTask();
+//
+//		/* Set RTC prescaler: set RTC period to 1sec */
+//		RTC_SetPrescaler(32767); /* RTC period = RTCCLK/RTC_PR = (32.768 KHz)/(32767+1) */
+//
+//		/* Wait until last write operation on RTC registers has finished */
+//		RTC_WaitForLastTask();
+//
+//		/* Set initial value */
+//		RTC_SetCounter( (uint32_t)((11*60+55)*60) ); // here: 1st January 2000 11:55:00
+//
+//		/* Wait until last write operation on RTC registers has finished */
+//		RTC_WaitForLastTask();
+//
+//		BKP_WriteBackupRegister(BKP_DR1, 0xA5A5);
+//
+//		/* Lock access to BKP Domain */
+//		PWR_BackupAccessCmd(DISABLE);
+//
+//	} else {
+//
+//		/* Wait for RTC registers synchronization */
+//		RTC_WaitForSynchro();
+//
+//	}
 
 	return 0;
 }
